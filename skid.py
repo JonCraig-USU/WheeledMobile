bbefae42a8a3387b9f78a2df164ff0d0414f0a12
JonCraig <jonathan.craig@aggiemail.usu.edu>
2021-09-24 17:27:58 -0600

    set up part b

--- skid.py
+++ skid.py
@@ -1,9 +1,9 @@
-from _typeshed import StrOrBytesPath
+import math
 import turtle as t
 import numpy as np
 
 # MAGIC NUMBERS
-scale = 1  # scale factor for windo use
+scale = 25  # scale factor for windo use
 
 w = .3     # width of the bot
 l = .5     # length of the bot
@@ -11,7 +11,7 @@ l = .5     # length of the bot
 vr = 0     # velocity of the right wheel
 vl = 0     # velocity of the left wheel
 
-step = .01   # time between calculats (seconds)
+step = .1   # time between calculats (seconds)
 
 # radians per second
 def phiDot(vl, vr, w):
@@ -30,19 +30,31 @@ left = [1, -1, .8, 2]                        # array with numbers for left wheel
 right = [1.5, -1.5, -2, 2]                   # array with numbers for right wheel velocities
 
 t.shape("turtle")
-t.shapesize(3, 5, 1)
+# t.shapesize(3, 5, 1)
+t.speed(15)
+t.penup
 
-for i in (0, len(left)):
+time = 0
+t.setheading(math.pi / 2)
+phi = math.pi / 2
+x = 0
+y = 0
+# dur = 0
+# for j in DoC:
+#     dur += j
+
+for i in range (0, len(left)):
     vl = left[i]
     vr = right[i]
-    dur = DoC[i]
+    dur = time + DoC[i]
 
-    for time in range(0, DoC, step):
+    while time < dur:
         phi = phiDot(vl, vr, w) * time
-        x = xDot(vl, vr, phi)
-        y = yDot(vl, vr, phi)
-        t.dot()
+        x += xDot(vl, vr, phi) * scale
+        y += yDot(vl, vr, phi) * scale
+        # t.dot()
         t.goto(x, y)
+        time += step
 
 t.mainloop()
 
